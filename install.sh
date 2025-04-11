a613f8616bf76d368079bd281dc3ce9ef17c174f
Areeb Ahmed <135235925+areebahmeddd@users.noreply.github.com>
2025-04-12 01:12:37 +0530

    docker fix

--- install.sh
+++ install.sh
@@ -67,6 +67,8 @@ install_docker() {
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" \
   | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+  apt-get update -y
+
   echo "Installing Docker..."
   apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   systemctl enable docker
